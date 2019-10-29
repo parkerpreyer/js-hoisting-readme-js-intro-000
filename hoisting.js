15cf0177967759949671fdb5f10abe7ef50de955
Parker Preyer <parker.preyer@gmail.com>
2019-10-29 16:19:17 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -19,11 +19,9 @@ function crazy() {
 function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
-
-  sayMy();
-
   function sayMy() {
     console.log(name);
     var name = "Kristin";
   }
+  sayMy();
 }
